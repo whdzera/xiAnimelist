89d8aa977717835da2582a0d326b4cf7c792f133
whdzera <whdzera@gmail.com>
2025-07-14 23:20:52 +0700

    update style anime card

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,5 @@ import { Application } from "@hotwired/stimulus";
 
 window.Stimulus = Application.start();
 
-import MessageController from "./controllers/message_controller.js";
-Stimulus.register("message", MessageController);
-import AnimeController from "./controllers/anime_controller.js"
+import AnimeController from "./controllers/anime_controller.js";
 Stimulus.register("anime", AnimeController);
--- app/javascript/controllers/message_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class MessageController extends Controller {
-  hide() {
-    this.element.remove();
-  }
-}
